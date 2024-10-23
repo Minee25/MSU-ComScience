8c807f498f320298625731b8fc34f7f8a13a9af7
Minee <78191030+Minee25@users.noreply.github.com>
2024-10-23 23:13:31 +0700

    Delete C Fresher1-1/C ปี1:1 ทำในห้อง directory

--- C Fresher1-1/C ปี1:1 ทำในห้อง/learn.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-                                                                               void helloworld(char *text) {printf("%s", text);}
-int main() {
-    helloworld("printf");
-
-
-    return 0;
-}
--- C Fresher1-1/C ปี1:1 ทำในห้อง/w15.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int n = 7;
-    char letter[n] = {'D', 'F', 'G', 'A', 'K', 'L', 'X'};
-
-
-}
